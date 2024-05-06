8a8e4d0205602784a73832300653f13e3a83c207
MariaZuany <mfzuany@miners.utep.edu>
2024-05-05 18:56:01 -0600

    finished files

---
+++ h/led.h
@@ -0,0 +1,8 @@
+#ifndef led_included
+
+#define led_included
+#define LED_GREEN BIT6
+#define LED_RED BIT0
+#define LEDS (BIT0 | BIT6)
+
+#endif
---
+++ h/p2InterruptHandler.c
@@ -0,0 +1,19 @@
+#include <msp430.h>
+
+#include <switches.h>
+
+
+
+//Switches on p2
+
+void __interrupt_vec(PORT2_VECTOR) Port_2(){
+
+  if (P2IFG & SWITCHES) {
+
+    P2IFG &= ~SWITCHES;
+
+    switch_interrupt_handler();
+
+  }
+
+}
