d9ca02592594740d18cdf12755a03820c8386e6d
MariaZuany <mfzuany@miners.utep.edu>
2024-05-05 20:27:38 -0600

    done?

--- project/wakedemo.c
+++ project/wakedemo.c
@@ -33,10 +33,10 @@ static char switch_update_interrupt_sense()
 
 void switch_init()
 {
-  p2ren |= switches;/* eNABLE RESISTORS FOR SWITCHES */
-  p2ie |= switches;/* eNABLE INTERRUPTS FROM SWITCHES */
-  p2out |= switches;/* pULL-UPS FOR SWITCHES */
-  p2dir &= ~switches;/* sET SWITCHES' BITS FOR INPUT */
+  P2REN |= SWITCHES;/* Enable resistors for switches */
+  P2IE |= SWITCHES;/* Enable interrupts from switches */
+  P2OUT |= SWITCHES;/* Pull-ups for switches */
+  P2DIR &= ~SWITCHES;
   switch_update_interrupt_sense();
 }
 
